d0671205b85b59c36b05f09f4168854904c79a3a
Ivan Bakaidov <ibakaidov@ya.ru>
2021-05-24 16:40:46 +0300

    fix  stupid err

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import utils from './utils'
 Vue.config.productionTip = false
 Vue.prototype.$utils = utils;
 
-const second = window.process.argv[window.process.argv.length - 1] === '1я]'
+const second = window.process.argv[window.process.argv.length - 1] === '1'
 
 
 import VuetifyDialog from 'vuetify-dialog'
