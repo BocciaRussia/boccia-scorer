7deae94f0d2e76b605ae9584904c93022d74776c
Ivan Bakaidov <ivan@aacidov.ru>
2021-12-19 15:03:28 +0300

    add women times

--- src/store/TIMECOOFS.ts
+++ src/store/TIMECOOFS.ts
@@ -1,15 +1,20 @@
 import { GClass, gclass } from "boccia-types/lib/GClass";
 
-export const TIMECOOFS:{
+export const TIMECOOFS: {
     [key in gclass]: number
 } = {
-    BC1: 300,
-    BC2: 240,
-    BC3: 360,
-    BC4: 240,
-    BC5: 240,
-    "ПBC3":420,
+    BCM1: 300,
+    BCW1: 300,
+    BCM2: 240,
+    BCW2: 240,
+    BCM3: 360,
+    BCW3: 360,
+    BCM4: 240,
+    BCW4: 240,
+    BCM5: 240,
+    BCW5: 240,
+    "ПBC3": 420,
     "ПBC4": 300,
     "ПBC5": 300,
-    "ТBC1/BC2": 360    
+    "ТBC1/BC2": 360
 } as const;
